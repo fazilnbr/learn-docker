01393a3dd07ebd8d2b92c096e894d75e0eaccb8c
fazilnbr <fazilkp2000@gmail.com>
2023-03-11 17:28:43 +0530

    add postgres
    add postgres to the docker image

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"github.com/fazilnbr/learn-docker/database"
 	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
+	database.ConnectDb()
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
